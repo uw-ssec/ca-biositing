4d81a49e6482eae2668f05fd4f43e494e261923c
petercarbsmith <petersmith@lbl.gov>
2025-12-08 10:50:11 -0700

    generated sqla from linkml.STILL NEEDS A CHECK.  Next up alembic.

---
+++ src/ca_biositing/datamodels/ca_biositing/datamodels/schemas/generated/places.py
@@ -0,0 +1,106 @@
+
+from sqlalchemy import Column, Index, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Geography(Base):
+    """
+    Geographic location.
+    """
+    __tablename__ = 'Geography'
+
+    geoid = Column(Text(), primary_key=True, nullable=False )
+    state_name = Column(Text())
+    state_fips = Column(Text())
+    county_name = Column(Text())
+    county_fips = Column(Text())
+    region_name = Column(Text())
+    agg_level_desc = Column(Text())
+
+
+    def __repr__(self):
+        return f"Geography(geoid={self.geoid},state_name={self.state_name},state_fips={self.state_fips},county_name={self.county_name},county_fips={self.county_fips},region_name={self.region_name},agg_level_desc={self.agg_level_desc},)"
+
+
+
+
+
+
+class BaseEntity(Base):
+    """
+    Base entity included in all main entity tables.
+    """
+    __tablename__ = 'BaseEntity'
+
+    id = Column(Integer(), primary_key=True, nullable=False )
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+    etl_run_id = Column(Text())
+    lineage_group_id = Column(Integer())
+
+
+    def __repr__(self):
+        return f"BaseEntity(id={self.id},created_at={self.created_at},updated_at={self.updated_at},etl_run_id={self.etl_run_id},lineage_group_id={self.lineage_group_id},)"
+
+
+
+
+
+
+class LookupBase(Base):
+    """
+    Base class for enum/ontology-like tables.
+    """
+    __tablename__ = 'LookupBase'
+
+    id = Column(Integer(), primary_key=True, nullable=False )
+    name = Column(Text())
+    description = Column(Text())
+    uri = Column(Text())
+
+
+    def __repr__(self):
+        return f"LookupBase(id={self.id},name={self.name},description={self.description},uri={self.uri},)"
+
+
+
+
+
+
+class LocationAddress(BaseEntity):
+    """
+    Physical address.
+    """
+    __tablename__ = 'LocationAddress'
+
+    geography_id = Column(Text())
+    address_line1 = Column(Text())
+    address_line2 = Column(Text())
+    city = Column(Text())
+    zip = Column(Text())
+    lat = Column(Float())
+    lon = Column(Float())
+    is_anonymous = Column(Boolean())
+    id = Column(Integer(), primary_key=True, nullable=False )
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+    etl_run_id = Column(Text())
+    lineage_group_id = Column(Integer())
+
+
+    def __repr__(self):
+        return f"LocationAddress(geography_id={self.geography_id},address_line1={self.address_line1},address_line2={self.address_line2},city={self.city},zip={self.zip},lat={self.lat},lon={self.lon},is_anonymous={self.is_anonymous},id={self.id},created_at={self.created_at},updated_at={self.updated_at},etl_run_id={self.etl_run_id},lineage_group_id={self.lineage_group_id},)"
+
+
+
+
+    # Using concrete inheritance: see https://docs.sqlalchemy.org/en/14/orm/inheritance.html
+    __mapper_args__ = {
+        'concrete': True
+    }
