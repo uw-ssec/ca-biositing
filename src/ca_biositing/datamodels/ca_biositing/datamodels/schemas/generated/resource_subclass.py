4b793dc760782ee7ff8333be22b861588455b2bd
petercarbsmith <petersmith@lbl.gov>
2025-12-30 08:55:42 -0700

    holiday work. New notebook for importing gsheeets to pandas and playing around

---
+++ src/ca_biositing/datamodels/ca_biositing/datamodels/schemas/generated/resource_subclass.py
@@ -0,0 +1,74 @@
+
+from sqlalchemy import Column, Index, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class BaseEntity(Base):
+    """
+    Base entity included in all main entity tables.
+    """
+    __tablename__ = 'base_entity'
+
+    id = Column(Integer(), primary_key=True, nullable=False )
+    created_at = Column(DateTime())
+    updated_at = Column(DateTime())
+    etl_run_id = Column(Text())
+    lineage_group_id = Column(Integer())
+
+
+    def __repr__(self):
+        return f"BaseEntity(id={self.id},created_at={self.created_at},updated_at={self.updated_at},etl_run_id={self.etl_run_id},lineage_group_id={self.lineage_group_id},)"
+
+
+
+
+
+
+class LookupBase(Base):
+    """
+    Base class for enum/ontology-like tables.
+    """
+    __tablename__ = 'lookup_base'
+
+    id = Column(Integer(), primary_key=True, nullable=False )
+    name = Column(Text())
+    description = Column(Text())
+    uri = Column(Text())
+
+
+    def __repr__(self):
+        return f"LookupBase(id={self.id},name={self.name},description={self.description},uri={self.uri},)"
+
+
+
+
+
+
+class ResourceSubclass(LookupBase):
+    """
+    Sub-classification of resources.
+    """
+    __tablename__ = 'resource_subclass'
+
+    id = Column(Integer(), primary_key=True, nullable=False )
+    name = Column(Text())
+    description = Column(Text())
+    uri = Column(Text())
+
+
+    def __repr__(self):
+        return f"ResourceSubclass(id={self.id},name={self.name},description={self.description},uri={self.uri},)"
+
+
+
+
+    # Using concrete inheritance: see https://docs.sqlalchemy.org/en/14/orm/inheritance.html
+    __mapper_args__ = {
+        'concrete': True
+    }
